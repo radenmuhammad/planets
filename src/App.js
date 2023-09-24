5e94a51a74e6a72d9aa19c216b7643976ab5883b
Raden <rma18feb91@gmail.com>
2023-09-25 01:59:50 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -18,21 +18,16 @@ function App() {
 
   return (
     <div className="App">
-      <h1 style={{ color: "green" }}>using JavaScript inbuilt FETCH API</h1>
       <center>
         {data.map((dataObj, index) => {
           return (
-            <div
-              style={{
-                width: "15em",
-                backgroundColor: "#35D841",
-                padding: 2,
-                borderRadius: 10,
-                marginBlock: 10,
-              }}
-            >
-              <p style={{ fontSize: 20, color: 'white' }}>{dataObj.name}</p>
-            </div>
+              <p>
+				 {dataObj.name}<br/>
+			     {dataObj.rotation_period}<br/>
+			     {dataObj.diameter}<br/>
+			     {dataObj.gravity}<br/>				 
+			  </p>
+
           );
         })}
       </center>
