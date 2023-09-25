f4aeab3809c3c3634d94895b0b57952fe7bf1e4b
Raden <rma18feb91@gmail.com>
2023-09-26 02:13:00 +0700

    second

--- src/App.js
+++ src/App.js
@@ -1,35 +1,76 @@
 import "./App.css";
 import React, { useState, useEffect } from "react";
+import ReactDOM from "react-dom";
+
+Array.prototype.unique = function() {
+    var a = this.concat();
+    for(var i=0; i<a.length; ++i) {
+        for(var j=i+1; j<a.length; ++j) {
+            if(a[i] === a[j])
+                a.splice(j--, 1);
+        }
+    }
+
+    return a;
+};
 
 function App() {
-  const url = "https://swapi.dev/api/planets/";
+  let url = "https://swapi.dev/api/planets/";
+  let response = [];
   const [data, setData] = useState([]);
-
-  const fetchInfo = () => {
-    return fetch(url)
-      .then((res) => res.json())
-      .then((d) => setData(d.results))
+  const [dataNext, setDataNext] = useState([]);
+  const fetchInfo = async () => {
+	  response = await fetch(
+		url
+	  ).then((response) => response.json());
+		var arrayCombine = data.concat(response.results).unique();	
+	  console.log(response);	
+	  setData(arrayCombine);	  
+	  setDataNext(response);
   }
-
+  const loadMore = (urlSelected) => {
+	  url = urlSelected;
+    fetchInfo();
+  }
 
   useEffect(() => {
     fetchInfo();
   }, []);
+    
+  
 
   return (
     <div className="App">
       <center>
-        {data.map((dataObj, index) => {
-          return (
-              <p>
+		{data.map((dataObj, index) => {
+		  return (
+			  <p>
 				 {dataObj.name}<br/>
-			     {dataObj.rotation_period}<br/>
-			     {dataObj.diameter}<br/>
-			     {dataObj.gravity}<br/>				 
+				 {dataObj.rotation_period}<br/>
+				 {dataObj.diameter}<br/>
+				 {dataObj.gravity}<br/>				 
+				 {dataObj.terrain}<br/>				 
+				 {dataObj.surface_water}<br/>				 
+				 {dataObj.population}<br/>
+				 Residence:
+				  {dataObj.residents.map(obj => {
+					return (
+						<h2>link: {obj}</h2>
+					);
+				  })}			
+				 Films:
+				  {dataObj.films.map(obj => {
+					return (
+						<h2>link: {obj}</h2>
+					);
+				  })}			
+				 {dataObj.created}<br/>
+				 {dataObj.edited}<br/>						 
 			  </p>
 
-          );
-        })}
+		  );
+		})}
+{dataNext.next != null && <button onClick={() => loadMore(dataNext.next)}>Load More</button>}		
       </center>
     </div>
   );
