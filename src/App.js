6e7b17558893c22c3e56b41ec18b4364c8ea725b
Raden <rma18feb91@gmail.com>
2023-09-28 15:51:52 +0700

    second commit

--- src/App.js
+++ src/App.js
@@ -2,125 +2,144 @@ import "./App.css";
 import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom";
 
-Array.prototype.unique = function() {
-    var a = this.concat();
-    for(var i=0; i<a.length; ++i) {
-        for(var j=i+1; j<a.length; ++j) {
-            if(a[i] === a[j])
-                a.splice(j--, 1);
-        }
-    }
-
-    return a;
-};
-
 function App() {
-  let url = "https://swapi.dev/api/planets/";
-  let response = [];
-  const [data, setData] = useState([]);
-  const [dataNext, setDataNext] = useState([]);
-  const [dataWishList, setDataWishList] = useState([]);  
-  const fetchInfo = async () => {
-	  response = await fetch(
-		url
-	  ).then((response) => response.json());
-		var arrayCombine = data.concat(response.results).unique();	
-	  console.log(response);	
-	  setData(arrayCombine);	  
-	  setDataNext(response);
-  }
-  const loadMore = (urlSelected) => {
-	  url = urlSelected;
-    fetchInfo();
-  }
-  
-
-  
-  const wishList = (dataObject) => {
-		dataObject['time'] = new Date().getTime();
-		localStorage.setItem(dataObject.name, JSON.stringify(dataObject));	  
-		var arrayCombine = [];
+	let url = "https://swapi.dev/api/planets/";
+	let response = [];
+	let arrayCombine = [];
+	Array.prototype.unique = function() {
+		var a = this.concat();
+		for(var i=0; i<a.length; ++i) {
+			for(var j=i+1; j<a.length; ++j) {
+				if(a[i] === a[j])
+					a.splice(j--, 1);
+			}
+		}
+		return a;
+	};  
+	const paging = 3;
+	const [data, setData] = useState([]);
+	const [dataNext, setDataNext] = useState([]);
+	const [dataWishList, setDataWishList] = useState([]);  
+	const [pagination, setPagination] = useState([]);  
+	
+	const fetchInfo = async () => {
+		response = await fetch(url).then((response) => response.json());
+		arrayCombine = data.concat(response.results).unique();	
+		setData(arrayCombine);	  
+		setDataNext(response);
+		loadWishList();
+	}
+	
+	const loadMore = (urlSelected) => {
+		url = urlSelected;
+		fetchInfo();
+	}
+	
+	const paginate = (number) =>{
+		
+	}
+	
+	const loadWishList = () => {
 		var time = [];
 		for (var i = 0; i < localStorage.length; i++){
 			var jsonObject = JSON.parse(localStorage.getItem(localStorage.key(i)));
 			time[jsonObject['time']] = jsonObject['name'];
 		}
 		time.sort();	
+		var a = 0;
 		for (let key of Object.keys(time)){
+			a ++;
 			arrayCombine.push(JSON.parse(localStorage.getItem(time[key])));
+			if( a > 2){
+				break;
+			}
 		}		
 		setDataWishList(arrayCombine);
-  }
-  
-  useEffect(() => {
-    fetchInfo();
-  }, []);
-    
-  
+		const result = [];		
+		for (let i = 1; i <  Math.ceil(localStorage.length/3); i++) {
+			result.push(i);
+		}	
+		console.log(result);	
+		setPagination(result);		
+	}  
 
-  return (
-    <div className="App">
-      <center>
-		{data.map((dataObj, index) => {
-		  return (
-			  <p>
-				 {dataObj.name}<br/>
-				 {dataObj.rotation_period}<br/>
-				 {dataObj.diameter}<br/>
-				 {dataObj.gravity}<br/>				 
-				 {dataObj.terrain}<br/>				 
-				 {dataObj.surface_water}<br/>				 
-				 {dataObj.population}<br/>
-				 Residence:
-				  {dataObj.residents.map(obj => {
-					return (
-						<h2>link: {obj}</h2>
-					);
-				  })}			
-				 Films:
-				  {dataObj.films.map(obj => {
-					return (
-						<h2>link: {obj}</h2>
-					);
-				  })}			
-				 {dataObj.created}<br/>
-				 {dataObj.edited}<br/>						 
-				 <input onClick={() => wishList(dataObj)} type="button" name="WishList" value="WishList"/>
-			  </p>
-		  );
-		})}
-{dataNext.next != null && <button onClick={() => loadMore(dataNext.next)}>Load More</button>}
-		 <br/>WishList: <br/>
-		{dataWishList.map((dataObj, index) => {
-		  return (
-			  <p>
-				 {dataObj.name}<br/>
-				 {dataObj.rotation_period}<br/>
-				 {dataObj.diameter}<br/>
-				 {dataObj.gravity}<br/>				 
-				 {dataObj.terrain}<br/>				 
-				 {dataObj.surface_water}<br/>				 
-				 {dataObj.population}<br/>
-				 Residence:
-				  {dataObj.residents.map(obj => {
-					return (
-						<h2>link: {obj}</h2>
-					);
-				  })}			
-				 Films:
-				  {dataObj.films.map(obj => {
-					return (
-						<h2>link: {obj}</h2>
-					);
-				  })}			
-				 {dataObj.created}<br/>
-				 {dataObj.edited}<br/>						 
-			  </p>
-		  );
-		})}		
-      </center>
-    </div>
-  );
+	const wishList = (dataObject) => {
+		dataObject['time'] = new Date().getTime();
+		localStorage.setItem(dataObject.name, JSON.stringify(dataObject));
+		loadWishList();	
+	}
+
+	useEffect((number) => {
+		fetchInfo();
+	}, []);
+	return (
+		<div className="App">
+		  <center>
+			{data.map((dataObj, index) => {
+			  return (
+				  <p>
+					 {dataObj.name}<br/>
+					 {dataObj.rotation_period}<br/>
+					 {dataObj.diameter}<br/>
+					 {dataObj.gravity}<br/>				 
+					 {dataObj.terrain}<br/>				 
+					 {dataObj.surface_water}<br/>				 
+					 {dataObj.population}<br/>
+					 Residence:
+					  {dataObj.residents.map(obj => {
+						return (
+							<h2>link: {obj}</h2>
+						);
+					  })}			
+					 Films:
+					  {dataObj.films.map(obj => {
+						return (
+							<h2>link: {obj}</h2>
+						);
+					  })}			
+					 {dataObj.created}<br/>
+					 {dataObj.edited}<br/>						 
+					 <input onClick={() => wishList(dataObj)} type="button" name="WishList" value="WishList"/>
+				  </p>
+			  );
+			})}
+			{dataNext.next != null && <button onClick={() => loadMore(dataNext.next)}>Load More</button>}
+			<br/>WishList: <br/>
+			{dataWishList.map((dataObj, index) => {
+			  return (
+				  <p>
+					 {dataObj.name}<br/>
+					 {dataObj.rotation_period}<br/>
+					 {dataObj.diameter}<br/>
+					 {dataObj.gravity}<br/>				 
+					 {dataObj.terrain}<br/>				 
+					 {dataObj.surface_water}<br/>				 
+					 {dataObj.population}<br/>
+					 Residence:
+					  {dataObj.residents.map(obj => {
+						return (
+							<h2>link: {obj}</h2>
+						);
+					  })}			
+					 Films:
+					  {dataObj.films.map(obj => {
+						return (
+							<h2>link: {obj}</h2>
+						);
+					  })}			
+					 {dataObj.created}<br/>
+					 {dataObj.edited}<br/>						 
+				  </p>
+			  );
+			})}		
+          {pagination.map((number) => (
+              <a onClick={() => paginate(number)} href="" className="page-link">
+                &nbsp;{number}
+              </a>
+          ))}			
+		  </center>
+		</div>
+	);
 }
 
 export default App;
